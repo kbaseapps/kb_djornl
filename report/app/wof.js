c81c67aab006e5bec35086e10f0f5fedba794fbe
Dakota Blair <dblair@bnl.gov>
2021-04-22 11:42:30 -0400

    Ensure use of the correct object name for state.
    This commit finalizes the state persistence functionality for reports in this
    app. This works by creating a new workspace object when the report is
    generated, and the workspace ref to this object is passed into the report. The
    report loads the state on initial load and then updates this object with state
    periodically as it is changed. This solution works well for an individual
    narrative, however the state will not, at present, survive a copy. This problem
    can be worked around by adding state export and import functionality. This will
    allow users to move state from one report to another without having to manually
    recover the state.

--- report/app/wof.js
+++ report/app/wof.js
@@ -3,6 +3,7 @@ import { Workspace } from './ws.js';
 export class WOF {
   constructor({ kbase_session, ref, url }) {
     this.kbaseSession = kbase_session; // eslint-disable-line camelcase
+    this.object = null;
     this.ref = ref;
     this.url = url;
     if (url) {
@@ -15,10 +16,15 @@ export class WOF {
 
   // get latest object ref
   async getLatestVersionRef() {
+    if (!this.ref) {
+      //eslint-disable-next-line no-console
+      console.error('The ref for this object is unset. Use setRef to do so.');
+    }
     const [wsid, objid] = this.ref.split('/').slice(0, 2);
     const listObjectsResponse = await this.ws.listObjects(wsid);
     const objects = listObjectsResponse.result[0];
-    const latestVer = objects.filter((object) => object[0] === parseInt(objid))[0][4];
+    this.object = objects.filter((object) => object[0] === parseInt(objid))[0];
+    const latestVer = this.object[4];
     return `${wsid}/${objid}/${latestVer}`;
   }
   // get stored nodes
@@ -36,11 +42,14 @@ export class WOF {
   }
   // update stored nodes
   async putStoredNodes(nodes) {
+    if (!this.object) {
+      this.getLatestVersionRef();
+    }
     const [wsid] = this.ref.split('/').slice(0, 1);
     const objects = [
       {
         type: 'KBaseNarrative.Metadata-3.0',
-        name: 'report-state',
+        name: this.object[1],
         data: {
           data_dependencies: [], // eslint-disable-line camelcase
           description: JSON.stringify(nodes),
