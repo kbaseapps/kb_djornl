242af7186c751a3525109ad3ad7a1304e22200ac
Dakota Blair <dblair@bnl.gov>
2021-08-20 15:58:15 -0400

    Added multiplex selection.
    This commit adds a choice of which set of multiplex networks to use via a
    selection menu in the RWR CV and RWR LOE apps. This also adds tests which run
    queries against each available multiplex network. There is some corruption in
    at least one of the data files in this iteration, that is, the All Layers file.

--- scripts/rwrtools-run.sh
+++ scripts/rwrtools-run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -x
 set -e
+set -x
 
 export RWR_TOOLS_REPO=${RWR_TOOLS_REPO:-'/kb/module/RWRtools/'}
+set +x
+echo Activate rwrtools conda environment
 source /miniconda/etc/profile.d/conda.sh
 conda activate rwrtools
+set -x
 mkdir -p /opt/work/tmp
 cd $RWR_TOOLS_REPO
 eval $RWR_TOOLS_COMMAND
